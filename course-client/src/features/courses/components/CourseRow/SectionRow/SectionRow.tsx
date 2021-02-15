7513997f8dd0ee8494149a25ab00e34828812842
Kyle Edgette <edgettek@gmail.com>
2021-02-15 13:06:35 -0500

    Update button label and disabled state if course has already started

--- course-client/src/features/courses/components/CourseRow/SectionRow/SectionRow.tsx
+++ course-client/src/features/courses/components/CourseRow/SectionRow/SectionRow.tsx
@@ -13,12 +13,16 @@ export const SectionRow = (props: Props) => {
     const { section } = props;
     const { nickname, startDate } = section;
 
+    const hasCourseStarted = dayjs(startDate).isBefore(dayjs());
+
+    const buttonLabel = hasCourseStarted ? "Start Date Passed" : "Add Course +";
+
     return (
         <div className="section-row-container">
             <h5>{nickname}</h5>
-            <div className="right-col">
-            <span>{`Start Date: ${dayjs(startDate).format("MMMM D, YYYY")}`}</span>
-            <button>Add Course +</button>
+            <div>
+                <span>{`Start Date: ${dayjs(startDate).format("MMMM D, YYYY")}`}</span>
+                <button disabled={hasCourseStarted}>{buttonLabel}</button>
             </div>
         </div>
     )
