92a3eef818f5274db98d962b77aec1fdce6dabd6
Kyle Edgette <edgettek@gmail.com>
2021-02-15 12:15:46 -0500

    Fetch all courses onMount in Courses component

--- course-client/src/components/Courses.js
+++ course-client/src/components/Courses.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axiosClient from "../axiosClient";
 
 export const Courses = ({ user }) => {
+
+	const [courses, setCourses] = useState({ loading: true });
+
+	useEffect(() => {
+		const fetchCourses = async () => {
+			return await axiosClient.get('/courses/');
+		};
+
+		fetchCourses().then((response) => {
+			setCourses(response.data)
+		}).catch((error) => {
+			setCourses(error.error)
+		});
+
+	}, [setCourses]);
+
+
 	return (
 		<>
 			<h1>Welcome: {user.username} </h1>
