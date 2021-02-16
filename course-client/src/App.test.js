02ea70a0b1ac763ccbb2347753c40aa5afd73ab7
Kyle Edgette <edgettek@gmail.com>
2021-02-16 17:30:01 -0500

    Add testing npm modules, add sample integration test

--- course-client/src/App.test.js
+++ course-client/src/App.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import MockAdapter from 'axios-mock-adapter';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
+import axiosClient from "./axiosClient";
 
-test('renders learn react link', () => {
+test('renders Login form initially', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginForm = getByText("Login");
+  expect(loginForm).toBeInTheDocument();
+});
+
+test('renders Courses page after successful login', () => {
+  const axiosMock = new MockAdapter(axiosClient);
+
+  const mockResponse = {
+    user: {
+      username: 'kedgette',
+      email: "wow@fake.com",
+      id: 10
+    }
+  };
+
+  // TO-DO: Mock does not work as expected
+  axiosMock.onPost("/login").reply(200, mockResponse);
+
+  const { getByPlaceholderText, getByText } = render(<App />);
+  const usernameField = getByPlaceholderText("Username");
+  const emailField = getByPlaceholderText("Email");
+
+  fireEvent.change(usernameField, { target: { value: 'kedgette'}});
+  fireEvent.change(emailField, { target: { value: 'wow@fake.com'}});
+
+  fireEvent.click(getByText('Log In'));
+
+  waitFor(() => {
+    expect(getByText("Courses")).toBeInTheDocument();
+    expect(getByText(`Welcome: kedgette`)).toBeInTheDocument();
+  });
+
 });
